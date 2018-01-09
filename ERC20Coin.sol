95425af9ac2c48ce1aede72a80ba988652230f28
Shouvik Mondal <mondal2shouvik@gmail.com>
2018-01-09 23:26:28 +0530

    added comments
    It has a fixed supply and only the creator can mint coins. The first transfer should be done by the creator in order to enable other network participants to use the coins. It is partially-ERC20-compliant as it does not implement the Approval(...) and Transfer(...) events.

--- ERC20Coin.sol
+++ ERC20Coin.sol
@@ -1,19 +1,21 @@
 pragma solidity ^0.4.0;
 
-contract Erc20Coin{
+contract ERC20Coin{
     mapping(address => uint) balances;
-    uint supply = 1000;
+    uint supply = 1000; //to keep the supply of coins fixed
     address master;
-    mapping(address => mapping(address => uint)) approved;
+    mapping(address => mapping(address => uint)) approved; 
     
     function Erc20Coin() public{
-        master = msg.sender;
+        master = msg.sender;  //master variable stores the network address when the contract gets deployed in the network 
     }
     
+    //ERC20 standard function to show the available supply of coins
     function totalSupply() public constant returns(uint){
         return supply;
     }
     
+    //Custom function to mint coins.Only 'master' i.e. the creator can mint coins for his/her own address
     function mint(uint noOfCoins) public{
         if(msg.sender == master && noOfCoins <= supply){
             balances[msg.sender] += noOfCoins;
@@ -23,11 +25,11 @@ contract Erc20Coin{
             throw;
         }
     }
-    
+    //ERC20 standard function to check balance 
     function balanceOf(address tokenOwner) public constant returns (uint balance){
         return balances[tokenOwner];
     }
-    
+    //ERC20 standard function to transfer coins from one address to another
     function transfer(address to, uint tokens) public returns (bool success){
         if(balances[msg.sender] >= tokens && tokens > 0){
             balances[to] += tokens;
@@ -36,7 +38,7 @@ contract Erc20Coin{
         }
         return false;
     }
-    
+    //ERC20 standard function to approve someone to spend specefic amount of coins on thier behalf 
     function approve(address spender, uint tokens) public returns (bool success){
         if(balances[msg.sender] > tokens){
             approved[msg.sender][spender] = tokens;
@@ -44,15 +46,15 @@ contract Erc20Coin{
         }
         return false;
     }
-    
+    //ERC20 standard function to show the approved amount of allowance for a spender 
     function allowance(address tokenOwner, address spender) public constant returns (uint remaining){
-        if(balances[tokenOwner] < approved[tokenOwner][spender]){
-            approved[tokenOwner][spender] = 0;
-            return approved[tokenOwner][spender];
+        if(balances[tokenOwner] < approved[tokenOwner][spender]){ //If owner has already exhausted the coins and does not have
+            approved[tokenOwner][spender] = 0;                    //sufficient balance for spender then the approved amount/allowance 
+            return approved[tokenOwner][spender];                 //gets nullified 
         }
         return approved[tokenOwner][spender];
     }
-    
+    //ERC20 standard function to allow spender to transfer allowed amount of coins to others 
     function transferFrom(address from, address to, uint tokens) public returns (bool success){
         if(balances[from] >= tokens && approved[from][msg.sender] >= tokens && tokens > 0){
             balances[to] += tokens;
